2ee3e5f5b490a7f1658fe22e18b1d7fd00055051
Tom Rosenblatt <rosenblatttom@gmail.com>
2018-03-03 19:57:27 -0800

    progress on hacker-news-clone

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import CreateLink from './CreateLink'
+import LinkList from './LinkList'
+import Header from './Header'
+import Login from './Login'
+import { Switch, Route } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/" component={LinkList} />
+          </Switch>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
